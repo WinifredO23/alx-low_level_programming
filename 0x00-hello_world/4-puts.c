96a3de7a8098dba8af0fdf726a6479a3188e3394
WinifredO23 <WinifredOnumbu@outlook.com>
2023-07-12 23:42:38 -0700

    puts printing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: prints message "programing is like a building"
+ * followed by new line using puts function
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
