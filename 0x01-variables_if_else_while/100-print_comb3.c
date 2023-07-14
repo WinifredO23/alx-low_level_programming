53cd339e51b48fd8817c7b9bf1a6a87096b132b8
WinifredO23 <WinifredOnumbu@outlook.com>
2023-07-14 15:16:04 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main	- Entry point of program
+ *
+ * Description:	print combo two
+ * Return: Always 0 (Sucess);
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 8; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if (a != 8 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
