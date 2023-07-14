1fcb5f13af2055479fb393476b3064adb67369dc
WinifredO23 <WinifredOnumbu@outlook.com>
2023-07-14 08:55:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main	- Entry point of the program
+ *
+ * Description:	print alphabet
+ * Return: Always 0 (Sucess);
+ */
+int main(void)
+{
+	char Alphabet;
+
+	for (Alphabet = 'a'; Alphabet <= 'z'; Alphabet++)
+	{
+		putchar(Alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
