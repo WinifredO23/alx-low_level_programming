4799fc0a2064d68f1b80f2a0288645fb83fd5273
WinifredO23 <WinifredOnumbu@outlook.com>
2023-07-14 08:36:59 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 	lastdg = n % 10;
 	if (lastdg > 5)
 	{
-		printf(" %d is greater than 5\n", lastdg);
+		printf(" %d and is greater than 5\n", lastdg);
 	}
 	else if (lastdg == 0)
 	{
-		printf(" %d is 0\n", lastdg);
+		printf(" %d and is 0\n", lastdg);
 	}
 	else
 	{
-		printf(" %d is less than 6 not 0\n", lastdg);
+		printf(" %d and is less than 6 not 0\n", lastdg);
 	}
 	return (0);
 }
