22d048b7962bb2e408e32181f7fa10a83edb1c0e
WinifredO23 <WinifredOnumbu@outlook.com>
2023-07-18 09:20:14 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two numbers returns result
+ * @c:	The first number
+ * @d:	The second number
+ * Return: Sum
+ */
+int add(int c, int d)
+{
+	return (c + d);
+}
