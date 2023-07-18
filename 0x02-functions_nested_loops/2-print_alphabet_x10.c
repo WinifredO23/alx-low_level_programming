dffa47ce7e61ddc1d01575be7639b5069ec8b68f
WinifredO23 <WinifredOnumbu@outlook.com>
2023-07-18 04:38:18 -0700

    first take

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints alphabet ten times
+ *
+ * Description:	Print x10
+ * Return: Void
+ */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	i++;
+	}
+}
