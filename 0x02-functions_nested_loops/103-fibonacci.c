3f93e824c93ddcdc86ae387bd51b41e04091e88f
WinifredO23 <WinifredOnumbu@outlook.com>
2023-07-18 11:37:50 -0700

    sum of fibonacci no

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry Point of program
+ *
+ * Description:	Prints the sum of fibnacci no
+ * Return: Alway 0
+ */
+int main(void)
+{
+	int f = 0;
+	long j = 1;
+	long h = 2;
+	long sum = h;
+
+	while (h + j < 4000000)
+	{
+	h += j;
+	if (h % 2 == 0)
+	{
+		sum += h;
+		j = h - j;
+	}
+	++f;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
