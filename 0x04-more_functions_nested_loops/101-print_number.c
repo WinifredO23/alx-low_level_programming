111caa12a308af6dbea1322324fb901719a62c5a
WinifredO23 <WinifredOnumbu@outlook.com>
2023-07-20 17:20:24 -0700

    print integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number -Prints integer
+ * @n:	The integer
+ * Return: 0
+ */
+void print_number(int n)
+{
+	int i = n;
+
+	if (n < 0)
+	{
+	n *= -1;
+	i = n;
+	_putchar('_');
+	}
+	i /= 10;
+	if (i != 0)
+	print_number(i);
+	_putchar((int) n % 10 + '0');
+}
