f228d9d3667cca4a5d221a230987730378c5899b
WinifredO23 <WinifredOnumbu@outlook.com>
2023-07-27 00:36:25 -0700

    number and letters

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - Replaces letters with num
+ * @str: String of letters to replace
+ * Return: Str
+ */
+char *leet(char *str) 
+{
+	char a[] = "aAeEoOtTlL";
+	char b[] = "443300771i";
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+	if (str[i] == a[j])
+	{
+	str[i] = b[j];
+	}
+	}
+	}
+	return (str);
+}
