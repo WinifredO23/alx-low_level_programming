5c50ca40c227bf7d0b2b2c992b8877f441fb1006
WinifredO23 <WinifredOnumbu@outlook.com>
2023-07-27 01:33:32 -0700

    hot fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
- * leet - Encode into 1337
- * @f: Input v
- * Return: Str
+ * leet - Encodes a string in 1337
+ * @str: String to be encoded
+ * Return: The resulting string;
  */
-char *leet(char *f)
+char *leet(char *str)
 {
-	char a[] = "aAeEoOtTlL";
-	char b[] = "443300771i";
 	int i, j;
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
 
-	for (i = 0; *(f + i); i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-	for (j = 0; j <= 9; j++)
+	for (j = 0; j < 10; j++)
 	{
-	if (f[i] == a[j])
+	if (str[i] == a[j])
 	{
-	f[i] = b[j];
+	str[i] = b[j];
 	}
 	}
 	}
-	return (f);
+	return (str);
 }
