604960a9e832e3c12bf434cf756d1db5089ddf7f
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-02 11:16:59 -0700

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,17 +13,19 @@ if (*s2 == '*')
 if (*(s2 + 1) == '\0')
 return (1);
 
-while (*s1 != '\0')
-{
+if (*s1 == '\0')
+return (wildcmp(s1, s2 + 1));
+
 if (wildcmp(s1, s2 + 1))
 return (1);
-s1++;
-}
+
+return (wildcmp(s1 + 1, s2));
 }
 else if (*s1 == *s2)
 {
 if (*s1 == '\0')
 return (1);
+
 return (wildcmp(s1 + 1, s2 + 1));
 }
 
