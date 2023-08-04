5027e785a71be517c2acc562b68a7b67ec6c51ea
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-04 01:39:25 -0700

    lib

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,9 +1,6 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include <unistd.h>
 int _putchar(char c)
 {
-	return (putchar(c));
+	return (write(1, &c, 1));
 }
