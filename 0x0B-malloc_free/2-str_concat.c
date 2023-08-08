1ec7f4729df869b29e7f6c3702e3d07bc9fc37cf
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-08 14:45:19 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,8 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
+	int l1 = 0;
+	int l2 = 0;
 
 	if (s1 == NULL)
 	{
@@ -19,12 +21,28 @@ char *str_concat(char *s1, char *s2)
 	{
 	s2 = "";
 	}
-	s = (char *)malloc(strlen(s1) + strlen(s2) + 1);
-
+	while (s1[l1] != '\0')
+	{
+	l1++;
+	}
+	while (s2[l2] != '\0')
+	{
+	l2++;
+	}
+	s = (char *)malloc(l1 + l2 + 1);
 	if (s != NULL)
 	{
-		strcpy(s, s1);
-		strcat(s, s2);
+	int i, j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	s[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	s[i + j] = s2[j];
+	}
+	s[i + j] = '\0';
 	}
 	return (s);
 }
