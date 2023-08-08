1f1acb7972049410f6a0ab991146dd3fb04ec1a1
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-08 10:56:09 -0700

    arg

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - Converts parameters to a string
+ * @ac: Argument count
+ * @av: Argument vector
+ * Return: Pointer to the concatenated string, or NULL on failure
+ */
+char *argstostr(int ac, char **av)
+{
+	int length = 0;
+	char *res;
+	int a, b, c;
+
+	if (ac == 0 || av == NULL)
+	{
+	return (NULL);
+	}
+
+	for (a = 0; a < ac; a++)
+	{
+	if (av[a] != NULL)
+	{
+	for (b = 0; av[a][b] != '\0'; b++)
+	{
+	length++;
+	}
+	length++;
+	}
+	}
+	res = (char *)malloc(length + ac + 1);
+	if (res != NULL)
+	{
+	c = 0;
+	for (a = 0; a < ac; a++)
+	{
+	if (av[a] != NULL)
+	{
+	for (b = 0; av[a][b] != '\0'; b++)
+	{
+	res[c] = av[a][b];
+	c++;
+	}
+	res[c] = '\n';
+	c++;
+	}
+	}
+	res[c] = '\0';
+	}
+	return (res);
+}
+
+
