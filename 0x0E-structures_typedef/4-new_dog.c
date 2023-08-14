481ff3a581fe97372ca91f47cece27e84859af2e
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-14 16:12:17 -0700

    p

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_d;
+	dog_t *new_dog;
 
 	if (name == NULL || owner == NULL)
 	{
@@ -20,27 +20,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	}
 
-	new_d = malloc(sizeof(dog_t));
-	if (new_d == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 	{
 	return (NULL);
 	}
 
-	new_d->name = strdup(name);
-	if (new_d->name == NULL)
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
 	{
-	free(new_d);
+	free(new_dog);
 	return (NULL);
 	}
 
-	new_d->owner = strdup(owner);
-	if (new_d->owner == NULL)
+	new_dog->owner = strdup(owner);
+	if (new_dog->owner == NULL)
 	{
-	free(new_d->name);
-	free(new_d);
+	free(new_dog->name);
+	free(new_dog);
 	return (NULL);
 	}
 
-	new_d->age = age;
-	return (new_d);
+	new_dog->age = age;
+	return (new_dog);
 }
