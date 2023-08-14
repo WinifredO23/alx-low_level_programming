86ff066008c85b3b5a2aa0f96f52daa8505176bb
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-14 15:56:56 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_d;
+
+	if (name == NULL || owner == NULL)
+	{
+	return (NULL);
+
+	}
+
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
+	{
+	return (NULL);
+	}
+
+	new_d->name = strdup(name);
+	if (new_d->name == NULL)
+	{
+	free(new_d);
+	return (NULL);
+	}
+
+	new_d->owner = strdup(owner);
+	if (new_d->owner == NULL)
+	{
+	free(new_d->name);
+	free(new_d);
+	return (NULL);
+	}
+
+	new_d->age = age;
+	return new_d;
+}
