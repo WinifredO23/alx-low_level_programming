7441f94422cbe019c8c3834e87eea3bb45edffe2
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-14 13:58:51 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - Atributes of poppy
+ * Description: Dog atrributes
+ * @name: Name of Poppy
+ * @age: Age
+ * @owner: Owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
