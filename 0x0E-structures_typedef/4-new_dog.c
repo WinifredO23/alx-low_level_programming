59a0ea61512b9878278ab9e8b6f7aa8cbcef3339
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-14 17:01:37 -0700

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * _strlen - Returns length of string
  * @s: String to evalauate
@@ -30,12 +29,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int a, j;
 
-	for(a = 0; src[a] != '\0'; a++)
+	for (a = 0; src[a] != '\0'; a++)
 	;
 
 	for (j = 0; j < a; j++)
 	{
-	dest[j] = src [j];
+	dest[j] = src[j];
 	}
 	dest[a] = '\0';
 
@@ -55,12 +54,10 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int a1, a2;
 
-	if (name == NULL || owner == NULL)
-	{
-	return (NULL);
-
-	}
+	a1 = _strlen(name);
+	a2 = _strlen(owner);
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
@@ -68,21 +65,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	return (NULL);
 	}
 
-	new_dog->name = strdup(name);
+	new_dog->name = malloc(sizeof(char) * (a1 + 1));
 	if (new_dog->name == NULL)
 	{
 	free(new_dog);
 	return (NULL);
 	}
 
-	new_dog->owner = strdup(owner);
+	new_dog->owner = malloc(sizeof(char) * (a2 + 1));
 	if (new_dog->owner == NULL)
 	{
-	free(new_dog->name);
 	free(new_dog);
+	free(new_dog->name);
 	return (NULL);
 	}
-
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
 	new_dog->age = age;
+
 	return (new_dog);
 }
