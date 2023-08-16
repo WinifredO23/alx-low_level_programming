ca3b42673e45abbc01495c7f4db92843f2dc6b3c
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-16 00:27:51 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - Prints a name
+ * @name: Name to print
+ * @f: Pointer
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+	f(name);
+	}
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_POINTERS_HEADER_H
+#define FUNCTION_POINTERS_HEADER_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
