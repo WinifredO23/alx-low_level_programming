0ecc6c40adb178c71aaf9046a3e892ed286e6f91
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-24 13:33:37 -0700

    length of list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
- * list_len: Prints Number of elements in list
+ * list_len - Prints Number of elements in list
  * @h: - Head of list
+ * Description:  Returns the number of elements in a linked list_t list.
  * Return: Number of elements
  */
 size_t list_len(const list_t *h)
