0f0a46b8ab5b3df7a0bdebb75e921ced33a3988a
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-31 09:07:09 -0700

    endianess

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_endianness - check endianness
+ * Return: 0 if big endian, or 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+
+	if (*(char *)&n == 1)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
