2342aee532172253d618da5c23db3549a668e3de
WinifredO23 <WinifredOnumbu@outlook.com>
2023-08-31 08:28:20 -0700

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * set_bit - sets value of a bit to 1 at a given index
+ * @n: decimal number passed by pointer
+ * @index: index position to change, starting from 0
+ * Return: 1 if it worked, -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int j = 1;
+
+	if (index > 65)
+	{
+		return (-1);
+	}
+
+	while (index > 0)
+	{
+		index--;
+		j *= 2;
+	}
+	*n += j;
+	return (1);
+
+}
+
