e323a1b348dafb3f76ef4f2897890f650b37c3e2
WinifredO23 <WinifredOnumbu@outlook.com>
2023-11-03 03:52:21 -0700

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Print a hash table
+ * @ht: The hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int printed = 0;
+	hash_node_t *current;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			if (printed == 1)
+			printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			printed = 1;
+			current = current->next;
+		}
+	}
+
+	printf("}\n");
+}
